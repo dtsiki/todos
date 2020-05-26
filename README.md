3f55db4ff79568cb67d6f0f3ccd2a69dd36cf218
Дарья <thevioletmaniac@gmail.com>
2020-05-26 15:27:28 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Hi! This is my to do app in React/Redux. 
+
+This project was bootstrapped with Create React App. 
+
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { render } from 'react-dom';
+import './App/style.css';
+import App from './App';
+
+render(
+  <App />,
+  document.getElementById('app'),
+);
+
+
+
+
