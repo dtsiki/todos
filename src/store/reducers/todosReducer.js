22f06462ac170a308568cec9c60f60e56ebdd8da
Дарья <thevioletmaniac@gmail.com>
2020-05-26 20:30:35 +0700

    redux todo states

--- src/store/reducers/todosReducer.js
+++ src/store/reducers/todosReducer.js
@@ -1,19 +1,35 @@
 const initialState = {
-  todos: [{id: 1, completed: false, text: "Drink a tea"}]
+  todos: [{id: 0, completed: false, text: "Drink a tea"}]
 };
 
 const todosReducer = (state = initialState, action) => {
   switch(action.type){
     case 'ADD_TODO':
+	  console.log(action.payload);
+	  console.log(action.id);
       return{
         ...state,
-        todos: [...state.todos, action.payload]
-      }	  
+        todos: [...state.todos, {id: action.id, completed: false, text: action.payload}]
+      }	
+	  
     case 'CLEAR_TODOS':
       return{
         ...state,
         todos: []
+      }	
+
+    case 'DELETE_TODO':
+      return {
+        ...state,
+        todos: [...state.todos.filter(todo => { return todo.id !== action.payload })]
+      }
+
+    case 'TOGGLE_TODO':
+      return {
+        ...state,
+        todos: [...state.todos, ...state.todos.filter(todo => {if(todo.id === action.payload) todo.completed = !todo.completed})]
       }	  
+	  
     default:
       return state;    
   }
